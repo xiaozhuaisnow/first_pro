44261f0733ccfd4bb97b649490d233b0acfea5ab
zhu penghui <hncyzph@163.com>
2017-03-20 14:02:52 +0800

    Test

---
+++ le1.py
@@ -0,0 +1,15 @@
+#!/usr/local/bin/python3
+# -*- coding: utf-8 -*-
+# print "这是中文"
+
+xx = """
+块注释
+"""
+# raw_input("\n\nthis is raw input")
+
+# import  sys; x = 'penghui5'; sys.stdout.write(xx + '\n');
+
+a = b = c = 2
+
+aa,bb,cc = 1,2,"penghui5"
+print(aa,bb,cc)
